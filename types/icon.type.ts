b31d8561ff2333e2078f36138cfa1f44c1ff468f
TonniPaul <ariyoadeoyepaul@gmail.com>
2026-06-01 12:44:09 +0100

    feat: add more image and svg icon sprite and updat icon name types

--- types/icon.type.ts
+++ types/icon.type.ts
@@ -1,1 +1,12 @@
-export type IconName = 'sun' | 'moon' | 'menu' | 'close';
+export type IconName =
+  | 'sun'
+  | 'moon'
+  | 'menu'
+  | 'close'
+  | 'loading'
+  | 'arrow-right-up'
+  | 'sparkles'
+  | 'instagram'
+  | 'github'
+  | 'linkedin'
+  | 'twitter';
