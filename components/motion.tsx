fb5ae4dcca99cb2abdf0f3bfb4ea4d9fa53cc448
TonniPaul <ariyoadeoyepaul@gmail.com>
2026-06-07 20:18:32 +0100

    feat: create motion component

--- components/motion.tsx
+++ components/motion.tsx
@@ -5,17 +5,38 @@ import { ElementType } from 'react';
 
 type MotionProps<T extends keyof HTMLElementTagNameMap> = {
   as?: T;
+  delay?: number;
 } & React.ComponentPropsWithoutRef<T> &
   React.ComponentProps<typeof motion.div>;
 
 function Motion<T extends keyof HTMLElementTagNameMap = 'div'>({
   as,
   children,
+  delay = 0,
+  initial = { opacity: 0, y: 40 },
+  whileInView = { opacity: 1, y: 0 },
+  viewport = { once: true, amount: 0.4 },
+  transition,
   ...props
 }: MotionProps<T>) {
   const Component = motion.create((as || 'div') as ElementType);
 
-  return <Component {...props}>{children}</Component>;
+  return (
+    <Component
+      initial={initial}
+      whileInView={whileInView}
+      viewport={viewport}
+      transition={{
+        duration: 1,
+        ease: 'easeOut',
+        delay,
+        ...transition,
+      }}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
 }
 
 export default Motion;
